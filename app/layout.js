331c0c28975534fe7da81f0f8015090d07331457
Lawrence Li <lonor@live.com>
2023-07-02 20:03:54 +0800

    feat: add icon

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,11 @@ import "./globals.css";
 import Providers from "./provider";
 
 export const metadata = {
-  title: "ChatGPT●",
+  title: "ChatGPT",
   description: "ChatGPT",
+  icons: {
+    icon: `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><circle cx="50" cy="50" r="50" fill="gray" /></svg>`,
+  },
 };
 
 export default function RootLayout({ children }) {
