7d9cd6d6176ab9844b149850b35e6847096be09f
Lawrence Li <lonor@live.com>
2023-09-17 12:45:41 +0800

    chore: fix apple icon

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
   description: "ChatGPT",
   icons: {
     icon: `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><circle cx="50" cy="50" r="50" fill="gray" /></svg>`,
-    apple: `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><circle cx="50" cy="50" r="50" fill="gray" /></svg>`,
+    apple: "/favicon.ico",
   },
 };
 
